4d418aefe99f942b3609d9dba956939464b6eb29
thais13bm <thais13bm@gmail.com>
2025-02-01 22:57:31 -0300

    nothing

--- microphone_dma.c
+++ microphone_dma.c
@@ -429,56 +429,7 @@ int main() {
     memset(ssd, 0, ssd1306_buffer_length);
     render_on_display(ssd, &frame_area);
 
-   //restart:
-
-// Parte do código para exibir a mensagem no display (opcional: mudar ssd1306_height para 32 em ssd1306_i2c.h)
-// /**
-    //ssd1306_draw_string(ssd, 5, 0, "guess what");
-    //render_on_display(ssd, &frame_area); 
-    /*char *text[] = {
-        "  Bem-vindos!   ",
-        "  Embarcatech   "};
-
-    int y = 0;
-    for (uint i = 0; i < count_of(text); i++)
-    {
-        ssd1306_draw_string(ssd, 5, y, text[i]);
-        y += 8;
-    }
-    render_on_display(ssd, &frame_area);  */
-
-
-
-  /*i2c_init(I2C_PORT, 400*1000);
-  
-  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-  gpio_pull_up(I2C_SDA);
-  gpio_pull_up(I2C_SCL);
-
-  ssd1306_t ssd;
-
-  ssd1306_init(&ssd, WIDTH, HEIGHT, false, 0x3C, I2C_PORT);
-  ssd1306_config(&ssd);
-
-  ssd1306_send_data(&ssd);
-
-  ssd1306_fill(&ssd, false);
-  ssd1306_hline(&ssd, 0, 127, 32, true);
-  ssd1306_vline(&ssd, 63, 0, 63, true);
-  //ssd1306_draw_string(&ssd, 0, 0, "Bem-vindos!");
-  //ssd1306_draw_string(&ssd, 0, 10, "Embarcatech");
-  ssd1306_send_data(&ssd); // Atualiza o display
-
-    ssd1306_draw_string(&ssd, 0, 0, "Bem-vindos!");
-    ssd1306_draw_string(&ssd, 0, 10, "Embarcatech");
-    ssd1306_send_data(&ssd); // Atualiza o display
-
-  ssd1306_send_data(&ssd);  */
-
-
-    
-
+   
 
   while (true) {
 
